7fcb9e0a76f5beb553c36927e741dac9cc3f876e
Jappa44 <jacob.noneofyourbussiness@gmail.com>
2025-11-14 15:04:22 -0500

    Update Lab11Prob01.java

--- Lab11Prob01.java
+++ Lab11Prob01.java
@@ -21,22 +21,35 @@ public class Lab11Prob01 {
 			DataInputStream input = new DataInputStream(new FileInputStream(new File("src/people.dat")));
 			DataOutputStream output = new DataOutputStream(new FileOutputStream(new File("src/people-copy.dat")));
 		) {
+			int age, zipCode;
+			String name, address;
+			double salary;
+			
+			
 			while (true) {
-				output.writeInt(input.readInt());
-				output.writeUTF(input.readUTF());
-				output.writeUTF(input.readUTF());
-				output.writeInt(input.readInt());
-				output.writeDouble(input.readDouble());
+				age = input.readInt();
+				name = input.readUTF();
+				address = input.readUTF();
+				zipCode = input.readInt();
+				salary = input.readDouble();
+				
+				System.out.printf("%d %s %s %d %.2f%n", age, name, address, zipCode, salary);
+				
+				output.writeInt(age);
+				output.writeUTF(name);
+				output.writeUTF(address);
+				output.writeInt(zipCode);
+				output.writeDouble(salary);
 			}
 		} catch (FileNotFoundException ex) {
 			System.out.println("Could not find the specified file.");
 			ex.printStackTrace();
 		} catch (EOFException ex) {
-			System.out.println("Reached end of file");
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (Exception e) {
 			System.out.println("An unexpected error occured.");
+			e.printStackTrace();
 		}
 	}
 }
