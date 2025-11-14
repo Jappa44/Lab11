6c1cd24a68e37e683ad24cb62b80ff13b9faeeac
Jappa44 <jacob.noneofyourbussiness@gmail.com>
2025-11-14 14:36:46 -0500

    Add files via upload

---
+++ Lab11Prob01.java
@@ -0,0 +1,13 @@
+/**
+ * File: Lab11Prob01.java
+ * @author Ben Bearth, Tyler Pridgen
+ * Created on: 11/14/2025
+ * Modified on: 11/14/2025
+ * Description:   
+ */
+
+public class Lab11Prob01 {
+	public static void main(String[] args) {
+
+	}
+}
